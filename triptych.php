f212390656d364789e6c57f91bd9ebd483d15be9
HK Kahng <hk@router-rooter.com>
2017-02-13 23:19:44 +0000

    add code to assign css class to panel based on left, middle, or right position

--- triptych.php
+++ triptych.php
@@ -16,7 +16,13 @@ get_template_part('templates/page', 'header'); ?>
 	           <?php while ( have_rows('triptych_panel') ) : the_row(); ?>
 	               <?php $panel_id++; ?>
                     <div class="triptych">
-                        <div class="triptych-panel-left">
+                        <div class="<?php if ($panel_id % 3 == 1) {
+                            echo "triptych-panel-left";
+                        } elseif ($panel_id % 3 == 2) {
+                            echo "triptych-panel-middle";
+                        } else {
+                            echo "triptych-panel-right";
+                        } ?>">
                           <div class="triptych-panel-title">
                             <?php the_sub_field('triptych_panel_title'); ?>
                           </div>
