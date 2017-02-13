ddab363a88b69c21f4a5709a319f475daf5a8332
HK Kahng <hk@router-rooter.com>
2017-02-13 23:11:22 +0000

    add code to give each panel its own id

--- triptych.php
+++ triptych.php
@@ -9,27 +9,24 @@ get_template_part('templates/page', 'header'); ?>
 
 <div id="primary">
 	<main id="main" class="site-main" role="main">
+        <h2><?php the_field('triptych_title'); ?></h2>
 	    <?php if( have_rows('triptych_panel') ): ?>
-	   
+	       <?php $panel_id = 0; ?>
 	       <div class="triptych-row">
-	           
 	           <?php while ( have_rows('triptych_panel') ) : the_row(); ?>
-	           
+	               <?php $panel_id++; ?>
                     <div class="triptych">
                         <div class="triptych-panel-left">
                           <div class="triptych-panel-title">
                             <?php the_sub_field('triptych_panel_title'); ?>
                           </div>
-                          <div class="triptych-panel-body">
+                          <div class="triptych-panel-body" id="triptych-panel-<?php echo $panel_id; ?>">
                             <?php the_sub_field('triptych_panel_content'); ?>
                           </div>
                         </div>
                     </div>
-        
         	    <?php endwhile; ?>
-	           
            </div>
-	        
         <?php endif; ?>
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
