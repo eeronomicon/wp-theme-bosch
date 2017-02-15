550a13ebe8b74831536e0e43c1cce878c3453afa
HK Kahng <hk@router-rooter.com>
2017-02-15 00:31:27 +0000

    enable toggling between image and text panel content, clicking disabled for panels without images

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,10 +1,8 @@
 (function($) {
 
 
-   $( ".triptych-panel-body" ).click(function() {
-      var clickTarget = "#" + $( this ).attr("id");
-      $( clickTarget ).toggle();
-      $( clickTarget + "-image" ).toggle();
+   $( ".triptych-panel-clickable" ).click(function() {
+      $( this).find(".triptych-panel-body").toggle();
     });
     
 
--- triptych.php
+++ triptych.php
@@ -16,23 +16,28 @@ get_template_part('templates/page', 'header'); ?>
 	           <?php while ( have_rows('triptych_panel') ) : the_row(); ?>
 	               <?php $panel_id++; ?>
                     <div class="triptych">
-                        <div class="<?php if ($panel_id == 1) {
-                            echo "triptych-panel-left";
-                        } elseif ($panel_id == 2) {
-                            echo "triptych-panel-middle";
+                      <?php if( get_sub_field('triptych_panel_image') ) {
+                          $panel_display = array('none','inline',' triptych-panel-clickable');
                         } else {
-                            echo "triptych-panel-right";
-                        } ?>">
+                          $panel_display = array('inline','none','');
+                        }
+                        ?>
+                        <div class="<?php if ($panel_id == 1) {
+                              echo "triptych-panel-left";
+                            } elseif ($panel_id == 2) {
+                              echo "triptych-panel-middle";
+                            } else {
+                              echo "triptych-panel-right";
+                            } ?> <?php echo $panel_display[2]; ?>">
                           <div class="triptych-panel-title">
                             <?php the_sub_field('triptych_panel_title'); ?>
                           </div>
-                          <div class="triptych-panel-body" id="triptych-panel-<?php echo $panel_id; ?>">
+                          
+                          <div class="triptych-panel-body" id="triptych-panel-<?php echo $panel_id; ?>-content" style="display:<?php echo $panel_display[0]; ?>;">
                             <?php the_sub_field('triptych_panel_content'); ?>
                           </div>
-                          <?php if( get_sub_field('triptych_panel_image') ): ?>
-                            <div class="triptych-panel-body" id="triptych-panel-<?php echo $panel_id; ?>-image" style="display:none; background-image:url(<?php echo the_sub_field('triptych_panel_image'); ?>)">
-                            </div>
-                          <?php endif; ?>
+                          <div class="triptych-panel-body" id="triptych-panel-<?php echo $panel_id; ?>-image" style="display:<?php echo $panel_display[1]; ?>; background-image:url(<?php echo the_sub_field('triptych_panel_image'); ?>)">
+                          </div>
                         </div>
                     </div>
         	    <?php endwhile; ?>
